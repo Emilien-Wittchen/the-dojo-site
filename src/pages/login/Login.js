43c7c294a9e9b65e3b3d1cf7cfb9a5bb80cdcb93
emilien-wittchen <abizw97@gmail.com>
2022-03-16 17:35:05 +0100

    creating user documents, logging users out

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,48 @@
+import {useState} from 'react';
 import './Login.css';
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className='auth-form' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          value={email}
+          required
+          type='email'
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          value={password}
+          required
+          type='password'
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </label>
+
+      {/* {!isPending && <button className='btn'>Login</button>}
+      {isPending && (
+        <button className='btn' disabled>
+          Loading
+        </button>
+      )}
+      {error && <div className='error'>{error}</div>} */}
+    </form>
+  );
 }
