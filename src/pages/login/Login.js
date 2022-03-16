97c44db9c08c3610b252d57bf0f410de64100f6c
emilien-wittchen <abizw97@gmail.com>
2022-03-16 18:08:50 +0100

    logging users in

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,16 @@
 import {useState} from 'react';
+import {useLogin} from '../../hooks/useLogin';
+
 import './Login.css';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const {login, error, isPending} = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
 
   return (
@@ -36,13 +39,13 @@ export default function Login() {
         />
       </label>
 
-      {/* {!isPending && <button className='btn'>Login</button>}
+      {!isPending && <button className='btn'>Login</button>}
       {isPending && (
         <button className='btn' disabled>
           Loading
         </button>
       )}
-      {error && <div className='error'>{error}</div>} */}
+      {error && <div className='error'>{error}</div>}
     </form>
   );
 }
