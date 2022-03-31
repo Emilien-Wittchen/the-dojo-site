aa7518ce429aac40a7f83434abd40b61197e84ce
emilien-wittchen <abizw97@gmail.com>
2022-03-31 17:29:25 +0200

    listing comments

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -21,19 +21,19 @@ const firestoreReducer = (state, action) => {
       };
     case 'DELETED_DOCUMENT':
       return {isPending: false, document: null, success: true, error: null};
-    case 'UPDATED_DOCUMENT':
-      return {
-        isPending: false,
-        document: action.payload,
-        success: true,
-        error: null,
-      };
     case 'ERROR':
       return {
         isPending: false,
         document: null,
         success: false,
         error: action.payload,
+      };
+    case 'UPDATED_DOCUMENT':
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
       };
     default:
       return state;
@@ -47,7 +47,7 @@ export const useFirestore = (collection) => {
   // collection ref
   const ref = projectFirestore.collection(collection);
 
-  // only dispatch is not cancelled
+  // only dispatch if not cancelled
   const dispatchIfNotCancelled = (action) => {
     if (!isCancelled) {
       dispatch(action);
@@ -79,7 +79,7 @@ export const useFirestore = (collection) => {
     }
   };
 
-  // update document
+  // update a document
   const updateDocument = async (id, updates) => {
     dispatch({type: 'IS_PENDING'});
 
@@ -90,8 +90,8 @@ export const useFirestore = (collection) => {
         payload: updatedDocument,
       });
       return updatedDocument;
-    } catch (err) {
-      dispatchIfNotCancelled({type: 'ERROR', payload: err.message});
+    } catch (error) {
+      dispatchIfNotCancelled({type: 'ERROR', payload: error});
       return null;
     }
   };
@@ -100,5 +100,5 @@ export const useFirestore = (collection) => {
     return () => setIsCancelled(true);
   }, []);
 
-  return {addDocument, deleteDocument, response};
+  return {addDocument, deleteDocument, updateDocument, response};
 };
